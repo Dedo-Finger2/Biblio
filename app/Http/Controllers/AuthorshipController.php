f13738aee8699e9420699fbbc75d1993f7bab397
Dedo_Finger2 <antonioimportant@gmail.com>
2023-07-25 14:14:55 -0300

    Models e controllers estrutura

---
+++ app/Http/Controllers/AuthorshipController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Authorship;
+use Illuminate\Http\Request;
+
+class AuthorshipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Authorship $authorship)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Authorship $authorship)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Authorship $authorship)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Authorship $authorship)
+    {
+        //
+    }
+}
